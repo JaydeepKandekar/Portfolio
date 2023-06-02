38636e91b7f1d81583653fce7d396f6281c390d4
Jaydeep Kandekar <99089964+JaydeepKandekar@users.noreply.github.com>
2023-06-02 15:58:58 +0530

    Update contact.php
    Signed-off-by: Jaydeep Kandekar <99089964+JaydeepKandekar@users.noreply.github.com>

--- contact.php
+++ contact.php
@@ -5,12 +5,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $country = $_POST['country'];
     $subject = $_POST['subject'];
 
+    // Email address to receive the form submissions
     $to = 'jaydeepkandekar8@gmail.com';
-    $subject = 'New Contact Form Submission';
-    $message = "First Name: $firstname\n\nLast Name: $lastname\n\nCountry: $country\n\nSubject: $subject";
+
+    // Subject of the email
+    $email_subject = 'New Contact Form Submission';
+
+    // Email content
+    $email_message = "First Name: $firstname\n\nLast Name: $lastname\n\nCountry: $country\n\nSubject: $subject";
+
+    // Headers
+    $headers = "From: $firstname $lastname <$to>" . "\r\n" .
+        "Reply-To: $to" . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
 
     // Send email
-    if (mail($to, $subject, $message)) {
+    if (mail($to, $email_subject, $email_message, $headers)) {
         echo 'Thank you for contacting us!';
     } else {
         echo 'Sorry, something went wrong. Please try again later.';
